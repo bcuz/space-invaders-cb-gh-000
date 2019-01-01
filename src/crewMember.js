15300f14a7828ff2999e8a2dffcc484aaa550abe
Adam N <adam.nawroc@gmail.com>
2019-01-01 14:44:20 +0000

    Automatically backed up by Learn

--- src/crewMember.js
+++ src/crewMember.js
@@ -18,6 +18,7 @@ class CrewMember {
     if (this.currentShip == 'Looking for a Rig') {
       return 'had no effect'
     } else {
+      console.log(this)
       this.currentShip.cloaked = true
     }
   }
