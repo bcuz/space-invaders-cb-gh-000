d8d4531842094d0320cb6480f6b6ae00e6f08f34
Adam N <adam.nawroc@gmail.com>
2019-01-01 13:49:00 +0000

    progress

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,12 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name) {
+      this.name = name;
+      this.phasers = 5;
+      this.shields = 4;
+      this.cloaked = false
+      this.warpDrive = 'disengaged'
+      this.docked = true
+      this.phasersCharge = 'uncharged'
+    }
+
+}
