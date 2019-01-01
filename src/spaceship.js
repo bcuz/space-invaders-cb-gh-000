4a291e9108414c229d1bf59637447c6f11f1fa5f
Adam N <adam.nawroc@gmail.com>
2019-01-01 14:23:57 +0000

    Automatically backed up by Learn

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,6 +1,7 @@
 class Spaceship {
   constructor(name, crew, phasers, shields) {
       this.name = name;
+      // infinite loop
       this.crew = crew.forEach(person => {person.currentShip = new Spaceship(this.name, crew, this.phasers, this.shields)})
       this.phasers = phasers;
       this.shields = shields;
