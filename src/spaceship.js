756f21ffe9f054c80185af97e45a7184a711f0dc
Adam N <adam.nawroc@gmail.com>
2019-01-01 14:04:49 +0000

    Automatically backed up by Learn

--- src/spaceship.js
+++ src/spaceship.js
@@ -5,7 +5,7 @@ class Spaceship {
       this.shields = shields;
       this.cloaked = false
       this.warpDrive = 'disengaged'
-      this.docked = crew.length > 0 ? true : false
+      this.docked = crew.length > 0 ? false : true
       this.phasersCharge = 'uncharged'
     }
 
