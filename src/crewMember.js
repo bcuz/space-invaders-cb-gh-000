5c8a7c9ba7e4a95dde01568d5dfc86e00788e726
Adam N <adam.nawroc@gmail.com>
2019-01-01 14:25:31 +0000

    Automatically backed up by Learn

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,6 +1,7 @@
 class CrewMember {
   constructor(position) {
       this.position = position;
+      // like i need a setter here or something.
       this.currentShip = 'Looking for a Rig';
     }
 
